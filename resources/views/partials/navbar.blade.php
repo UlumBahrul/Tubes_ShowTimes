065f4d176cef8701e26e88445c25c8ef4353067a
UlumBahrul <203040089@mail.unpas.ac.id>
2022-12-15 02:26:02 +0700

    membuat user registration

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,6 +5,7 @@
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+      
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
@@ -17,15 +18,18 @@
             <a class="nav-link {{ ($title === "Contact")?'active': ''}}" href="#">Contact</a>
           </li>
         </ul>
+        
         <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
+        
         <ul class="navbar-nav mb-2 mb-lg-0">
           <li class="nav-item">
-            <a href="/login" class="nav-link "><i class="bi bi-box-arrow-in-right"></i>Login</a>
+            <a href="/login" class="nav-link {{ ($title === "login")?'active': ''}}"><i class="bi bi-box-arrow-in-right"></i>Login</a>
           </li>
         </ul>
+    
       </div>
     </div>
   </nav>
