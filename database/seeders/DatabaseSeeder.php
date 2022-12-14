48a9b611fea2de84ff492952ece83956b9e4b03b
UlumBahrul <203040089@mail.unpas.ac.id>
2022-12-14 22:57:16 +0700

    membuat database dengan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +23,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Bahrul ulum',
+            // 'username' => 'bahulum',
+            'email' => 'bahulum97@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        // User::factory(4)->create();
+
+
+        // Post::factory(10)->create();
     }
 }
