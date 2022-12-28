ca7e788ac0a4a72328658520c8fb62b16e1f46b5
PaizAJ <johndoe@example.com>
2022-12-28 19:32:41 +0700

    admin

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -8,11 +8,25 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ Request::is('dashboard/posts*') ? 'active' : '' }}" href="/dashboard/posts">
+          <a class="nav-link {{ Request::is('dashboard/posts') ? 'active' : '' }}" href="/dashboard/posts">
             <span data-feather="file-text"></span>
             My Posts
           </a>
         </li>
       </ul>
+
+      <h6 class="sidebar-heading d-flex justify-content-beetween align-item-center px-3 mt-4 mb-1 text-muted">
+        <span>Administratior</span>
+      </h6>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href="/dashboard/categories">
+            <span data-feather="grid"></span>
+            Post Categories
+          </a>
+        </li>
+      </ul>
+      
+      
     </div>
   </nav>
--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -17,6 +17,24 @@
                 <th scope="col">Action</th>
                 </tr>
             </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->category->name }}</td>
+                <td>
+                    <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
+                    <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+                    <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                        @method('delete')
+                        @csrf
+                        <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span></button>
+                        </form>
+                </td>
+                </tr>
+                @endforeach
+            </tbody>
         </table>
     </div>
 @endsection
