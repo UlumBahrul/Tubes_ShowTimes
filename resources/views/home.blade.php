488320b1db05c999ec92c228c5aee28b6eb6de1f
MRafliansyah <203040105@mail.unpas.ac.id>
2022-12-30 07:59:44 +0700

    menambahkan icon website dan keterangan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
   <div class="header_left">
       <div class="banner_main">
           <h1 class="banner_taital">IT'S SHOW TIME</h1>
-          <p class="banner_text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever </p>
+          <p class="banner_text">Hi, Welcome to gallery
+            This gallery is a place that serves to present and exhibit works of art to be communicated to the wider community. </p>
           <div class="btn_main">
               <div class="more_bt"><a href="/blog">See More </a></div>
           </div>
@@ -13,6 +14,7 @@
   </div>
   <div class="header_right">
       <img src="/img/logo.png" class="banner_img">
+      <link rel="icon" href="img/icon.ico"/>
   </div>
 </div>
 
